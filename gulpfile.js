c7947875810eee7fc3559dcae7d72cb3688ea20b
Alex Symonds <alex-symonds@gmail.com>
2022-10-21 15:13:26 +0100

    Add dist files to repo

--- gulpfile.js
+++ gulpfile.js
@@ -13,11 +13,27 @@ const PATH_DIST_CSS = 'dist/static/styles';
 const PATH_JS_FILES = 'dev/scripts';
 const PATH_DIST_JS = 'dist/static/scripts';
 
-function css(){
+function cssMain(){
+  // return css(PATH_CSS_FILES, PATH_DIST_CSS);
+
   return gulp.src([`${PATH_CSS_FILES}/*.css`])
   .pipe(concat('styles.css'))
   .pipe(cleanCSS())
   .pipe(gulp.dest(PATH_DIST_CSS));
+}
+
+function cssMonsters(){
+  return css('dist/monsters/static/css', 'dist/monsters/static/styles');
+}
+
+function cssDoodles(){
+  return css('dist/doodles/static/css', 'dist/doodles/static/styles');
+}
+
+function css(SRC_PATH, DST_PATH){
+  return gulp.src([`${SRC_PATH}/*.css`])
+  .pipe(cleanCSS())
+  .pipe(gulp.dest(DST_PATH));
 }
 
 function jsMain() {
@@ -29,6 +45,8 @@ function jsMain() {
 }
 
 exports.default = function(){
-    watch('dev/css/*.css', css);
+    watch('dev/css/*.css', cssMain);
     watch('dev/scripts/*js', jsMain);
+    watch('dist/monsters/static/css', cssMonsters);
+    watch('dist/doodles/static/css', cssDoodles);
 };
